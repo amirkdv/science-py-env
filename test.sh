06ec1b7ebc3cc908b96296e6f3e16a28d97b4057
Amir Kadivar <amir@amirkdv.ca>
2016-06-27 15:40:18 -0400

    more verbose test

--- test.sh
+++ test.sh
@@ -4,4 +4,14 @@ virtualenv env
 . env/bin/activate
 
 pip install numpy scipy matplotlib
-python -c 'import numpy, scipy, matplotlib'
+
+{
+cat <<EOF
+import numpy, scipy, matplotlib
+
+print('numpy: ', numpy.__version__)
+print('scipy: ', scipy.__version__)
+print('matplotlib', matplotlib.__version__)
+EOF
+} | python -
+
